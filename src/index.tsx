02d285a4129d1ab1d86e9c0d06c1272ec5d6b994
ankush-UWC <154612245+ankush-UWC@users.noreply.github.com>
2023-12-23 15:28:57 +0530

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import  App  from './components/App';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App';
 
-document.body.onload = function () {
-    // Attach the React app root component to document.body
-    createRoot(document.body).render(<App />);
-};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
